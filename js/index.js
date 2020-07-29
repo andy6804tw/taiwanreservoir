7e5ac6e7d489b7c952b9bfaa80c1748929826379
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-07-29 15:10:27 +0800

    add dynamic render data

--- js/index.js
+++ js/index.js
@@ -33,34 +33,53 @@ const initProgress = () => {
 
 const reservoirItem = document.getElementById('reservoirItem');
 let reservoirItemHtml = '';
-for (let i = 0; i < 21; i++) {
-    const percent=Math.floor(Math.random() * 100)+1; 
-    reservoirItemHtml += `
-    <div class="col-sm-6 col-md-4 col-lg-3 my-4">
-                    <h5>新山水庫(基隆)</h5>
-                    <div class="box" percent="${percent}">
-                        <div class="percent">
-                            <div class="percentNum" id="count_${i}">0</div>
-                            <div class="percentB">%</div>
+// 疫情統計
+axios.get(`https://www.taiwanstat.com/waters/latest`)
+    .then((response) => {
+        var dataObject = response.data;
+        const reservoirList=['新山水庫','翡翠水庫','石門水庫','永和山水庫','寶山水庫','寶山第二水庫','明德水庫','鯉魚潭水庫'
+        ,'德基水庫','石岡壩','日月潭水庫','霧社水庫','湖山水庫','仁義潭水庫','蘭潭水庫'
+        ,'白河水庫','曾文水庫','烏山頭水庫','南化水庫','阿公店水庫','牡丹水庫']
+        console.log(Object.keys(dataObject[0]).length)
+        // render DOM
+        for (let i = 0; i < Object.keys(dataObject[0]).length; i++) {
+            const percent = Math.floor(Math.random() * 100) + 1;
+            const name=reservoirList[i];
+            const percentage=Math.floor(dataObject[0][reservoirList[i]].percentage);
+            const volumn=dataObject[0][reservoirList[i]].volumn;
+            const daliyInflow= dataObject[0][reservoirList[i]].daliyInflow;
+            const updateAt=dataObject[0][reservoirList[i]].updateAt;
+            reservoirItemHtml += `
+            <div class="col-sm-6 col-md-4 col-lg-3 my-4">
+                            <h5>${name}</h5>
+                            <div class="box" percent="${percentage}">
+                                <div class="percent">
+                                    <div class="percentNum" id="count_${i}">0</div>
+                                    <div class="percentB">%</div>
+                                </div>
+                                <div id="water_${i}" class="water">
+                                    <svg viewBox="0 0 560 20" id="water_wave_back_${i}" class="water_wave water_wave_back">
+                                        <use xlink:href="#wave"></use>
+                                    </svg>
+                                    <svg viewBox="0 0 560 20" id="water_wave_front_${i}" class="water_wave water_wave_front">
+                                        <use xlink:href="#wave"></use>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div class="info">
+                                <span>有效蓄水量：${volumn}萬立方公尺</span><br>
+                            <span>今日進水量：${daliyInflow}萬立方公尺</span><br>
+                            <span>更新時間：${updateAt}</span>
+                            </div>
                         </div>
-                        <div id="water_${i}" class="water">
-                            <svg viewBox="0 0 560 20" id="water_wave_back_${i}" class="water_wave water_wave_back">
-                                <use xlink:href="#wave"></use>
-                            </svg>
-                            <svg viewBox="0 0 560 20" id="water_wave_front_${i}" class="water_wave water_wave_front">
-                                <use xlink:href="#wave"></use>
-                            </svg>
-                        </div>
-                    </div>
-                    <div class="info">
-                        <span>有效蓄水量：842.40萬立方公尺</span><br>
-                    <span>昨日水量上升：0.67%</span><br>
-                    <span>預測剩餘天數：----</span><br>
-                    <span>更新時間：2020-07-28(8時)</span>
-                    </div>
-                </div>
-    `
-}
+            `
+        }
+        reservoirItem.innerHTML = reservoirItemHtml;
+        initProgress();
+    },
+        (error) => {
+            var message = error.response.data.message;
+        }
+    );
 
-reservoirItem.innerHTML = reservoirItemHtml;
-initProgress();
+
